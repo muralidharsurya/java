56ea50f962643a00941c806bb0c5f8f50019bc86
muralidharsurya <smuralidar4ee@gmail.com>
2022-07-07 17:57:59 +0530

    Addition method added

---
+++ Javalearning/src/com/methods/Addition.java
@@ -0,0 +1,16 @@
+package com.methods;
+
+public class Addition {
+    public static void main(String[] args) {
+         addition();
+        System.out.println(addition());
+    }
+    public static int addition(){
+        int a = 19;
+        int b = 18;
+        int sum =a+b;
+        return sum;
+
+    }
+
+}
