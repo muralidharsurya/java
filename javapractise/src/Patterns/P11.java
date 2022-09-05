6cddd23e08ac0169f7d223e08c757d94c497feb2
muralidharsurya <smuralidar4ee@gmail.com>
2022-09-05 05:57:06 +0530

    java

---
+++ javapractise/src/Patterns/P11.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class P11 {
+    public static void main(String[] args) {
+
+        for(int row=1;row<=4;row++) {
+            for (int col = 1; col <= 4; col++) {///888
+
+                if (row<=col)
+                    System.out.print("*  ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }}
+}
