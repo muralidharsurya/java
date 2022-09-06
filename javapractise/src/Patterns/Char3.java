7bcd334b14f9b68c79816340780ce57ef2a39322
muralidharsurya <smuralidar4ee@gmail.com>
2022-09-06 11:22:53 +0530

    patterns

---
+++ javapractise/src/Patterns/Char3.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class Char3 {
+    public static void main(String[] args) {
+        for (char row='A';row<='C';row++){
+            for (char col='A';col<='C';col++){
+                System.out.print(col+" ");
+
+            }
+            System.out.println();
+        }    }
+
+    }
+
