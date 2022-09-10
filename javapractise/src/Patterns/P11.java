9e11667646f8f494936d50b44871b63099eb7dbb
muralidharsurya <smuralidar4ee@gmail.com>
2022-09-10 21:18:56 +0530

    Patterns

--- javapractise/src/Patterns/P11.java
+++ javapractise/src/Patterns/P11.java
@@ -2,7 +2,6 @@ package Patterns;
 
 public class P11 {
     public static void main(String[] args) {
-
         for(int row=1;row<=4;row++) {
             for (int col = 1; col <= 4; col++) {///888
 
