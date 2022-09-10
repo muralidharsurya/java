9ba874a979f5a2b76c0c6feb5cb2c4f872e73a37
muralidharsurya <smuralidar4ee@gmail.com>
2022-09-10 21:21:52 +0530

    Ptterns

---
+++ javapractise/src/Patterns2/Pattern_Square.java
@@ -0,0 +1,21 @@
+package Patterns2;
+
+public class Pattern_Square {
+    public static void main(String[] args) {
+        for(int row=1;row<=5;row++){
+            for (int col=1;col<=5;col++){
+                if(row==1||col==1||row==5||col==5){
+                    System.out.print("*");
+                    }
+                    else {
+                    System.out.print(" ");
+
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
+    }
+
