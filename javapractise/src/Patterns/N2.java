ee23f3a8ce38e7737251a197e77fe810087fe91a
muralidharsurya <smuralidar4ee@gmail.com>
2022-09-10 20:49:18 +0530

    patterns

---
+++ javapractise/src/Patterns/N2.java
@@ -0,0 +1,15 @@
+package Patterns;
+
+public class N2 {
+
+        public static void main(String[] args) {
+            for (int row=1;row<=3;row++){
+                for (int col=1;col<=3;col++){
+                    System.out.print(col);
+
+                }
+                System.out.println();
+            }
+        }
+    }
+
