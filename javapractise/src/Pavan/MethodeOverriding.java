7081f9713a00834c29e0856fe08be81975a3c826
muralidharsurya <smuralidar4ee@gmail.com>
2022-09-11 10:02:01 +0530

    methodover

--- javapractise/src/Pavan/MethodeOverriding.java
+++ javapractise/src/Pavan/MethodeOverriding.java
@@ -1,17 +1,18 @@
 package Pavan;
 
-class Bank{
+class Bank{ // if sub class (chaild class)has the same method as declared in parent class
     double rateofInterest(){
         return 0;
     }
 }
 class SBI extends Bank{
       double rateofInterest(){
-          return 23.5;
+          return 24.5;
       }
 }
 class ICICI extends Bank{
-    double  rateofInterest(){
+    double  rateofInterest()
+    {
         return 98.7;
     }
 }
